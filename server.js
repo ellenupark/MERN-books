99d5692bbe000f6c8de84e4412c1fa124961d14c
ellenupark <ellenupark@gmail.com>
2021-02-26 03:00:42 -0500

    Created frontend and connected database

--- server.js
+++ server.js
@@ -1,18 +1,37 @@
 const express = require('express');
+const routes = require('./routes');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+require('dotenv').config();
+
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+require('./models/Book');
+
 // configure body parser for AJAX requests
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 app.use(express.json());
 
+app.use(express.static('client/build'));
+
+
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log("MongoDB database connection established successfully");
+})
+
 // routes
-app.get('/', (req, res) => {
-	res.send('Hello from MERN');
-});
 
 // Bootstrap server
 app.listen(PORT, () => {
 	console.log(`Server listening on port ${PORT}.`);
 });
+
+app.use(routes);
